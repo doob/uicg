94d46769f2460a64a84d744abe45dec202b4a764
Dennis Söderström <dennis.soderstrom@gmail.com>
2024-03-03 19:52:38 +0100

    fix: clean cli, add options

--- apps/cli/cli.js
+++ apps/cli/cli.js
@@ -1,81 +1,105 @@
 #!/usr/bin/env node
 
-import { program } from "commander";
-import inquirer from "inquirer";
-import fs from "fs";
-import path from "path";
-import chalk from "chalk";
-import add from "./src/commands/add.js";
-import { fileURLToPath } from "url";
-import { checkConfig, checkPackageExists, cyan, error, fileExists, findConfigFile, getConfig, getJsConfig, green, libPath, localPath, readFileSync, success, warning } from "./src/utils.js";
-import { verifyCompilerOptions, verifyProjectDependencies, writeConfigFile } from "./src/lib.js";
+import chalk from 'chalk'
+import { Command } from 'commander'
+import fs from 'fs'
+import inquirer from 'inquirer'
+import path from 'path'
+import { fileURLToPath } from 'url'
+import add from './src/commands/add.js'
+import {
+  verifyCompilerOptions,
+  verifyProjectDependencies,
+  writeConfigFile,
+} from './src/lib.js'
+import {
+  checkConfig,
+  error,
+  fileExists,
+  libPath,
+  readFileSync,
+} from './src/utils.js'
 
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
-const packageJson = JSON.parse(readFileSync("../package.json"));
+const program = new Command()
+const packageJson = JSON.parse(readFileSync('../package.json'))
 
-program.name("ui-gen").description("CLI for generating UI components").version(packageJson.version);
-
-program
-  .command("init")
-  .description("Initialize a new ui component project")
-  .action(() => {
-    const questions = [
-      {
-        type: "input",
-        name: "outputComponents",
-        message: "Where do you want to store your components?",
-        default: "src/components",
-      },
-      {
-        type: "input",
-        name: "aliasComponents",
-        message: "Do you want to use alias for your components?",
-        default: "@components",
-      },
-    ];
-
-    inquirer.prompt(questions).then(async (answers) => {
-      await verifyProjectDependencies();
-      await verifyCompilerOptions(answers);
-      await writeConfigFile(answers);
-
-      console.log("\n");
-      console.log(`Run ${chalk.blueBright.bold("npx ui-gen add button")} to generate a component`);
-      console.log(`Run ${chalk.blueBright.bold("npx ui-gen list")} to se a list of available components`);
-    });
-  });
+const install = async (answers, options) => {
+  await verifyProjectDependencies()
+  await verifyCompilerOptions(answers, options)
+  await writeConfigFile(answers)
+}
 
 program
-  .command("add <component>")
-  .description("Generate a component")
-  .action(async (component) => {
-    await checkConfig();
+  .name('ui-gen')
+  .description('CLI for generating UI components')
+  .version(packageJson.version)
 
-    const templatePath = fileExists("templates", `${component}.astro`);
-    if (!templatePath) {
-      console.log(error(`Missing component template: ${component}`));
-      process.exit(1);
+program
+  .command('init')
+  .option('-y, --yes')
+  .option('-d, --debug', 'output extra debugging')
+  .description('Initialize a new ui component project')
+  .action((options) => {
+    let questions = [
+      {
+        message: 'Where do you want to store your components?',
+        type: 'input',
+        name: 'outputComponents',
+        default: 'src/components',
+      },
+      {
+        message: 'Do you want to use alias for your components?',
+        type: 'input',
+        name: 'aliasComponents',
+        default: '@components',
+      },
+    ]
+
+    if (options.yes) {
+      const answers = {}
+      for (const q of questions) {
+        answers[q.name] = q.default
+      }
+
+      return install(answers)
     }
 
-    const { docs } = add(component);
-
-    console.log(docs);
-  });
+    inquirer.prompt(questions).then(async (answers) => {
+      await install(answers)
+    })
+  })
 
 program
-  .command("list")
-  .description("List available components")
+  .command('add <component>')
+  .description('Generate a component')
   .action(async (component) => {
-    const directoryPath = libPath("templates");
-    const files = fs.readdirSync(directoryPath);
+    await checkConfig()
 
-    console.log(chalk.bold("\nAvailable components:"));
+    const templatePath = fileExists('templates', `${component}.astro`)
+    if (!templatePath) {
+      console.log(error(`Missing component template: ${component}`))
+      process.exit(1)
+    }
+
+    const { docs } = add(component)
+
+    console.log(docs)
+  })
+
+program
+  .command('list')
+  .description('List available components')
+  .action(async (component) => {
+    const directoryPath = libPath('templates')
+    const files = fs.readdirSync(directoryPath)
+
+    console.log(chalk.bold('\nAvailable components:'))
     console.log(
       files
-        .filter((f) => f.includes(".astro"))
-        .map((f) => f.replace(".astro", ""))
-        .join("\n")
-    );
-  });
+        .filter((f) => f.includes('.astro'))
+        .map((f) => f.replace('.astro', ''))
+        .join('\n')
+    )
+  })
 
-program.parse(process.argv);
+program.parse(process.argv)
--- apps/cli/src/lib.js
+++ apps/cli/src/lib.js
@@ -1,21 +1,30 @@
-import chalk from "chalk";
-import { execSync } from "child_process";
-import fs from "fs";
-import handlebars from "handlebars";
-import inquirer from "inquirer";
-import path from "path";
-import { fileURLToPath } from "url";
-import { checkPackageExists, findConfigFile, localPath, success } from "./utils.js"; // Add missing import statement
+import chalk from 'chalk'
+import { execSync } from 'child_process'
+import fs from 'fs'
+import handlebars from 'handlebars'
+import inquirer from 'inquirer'
+import path from 'path'
+import { fileURLToPath } from 'url'
+import {
+  checkPackageExists,
+  findConfigFile,
+  localPath,
+  success,
+} from './utils.js' // Add missing import statement
 
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
 /**
  * Verify project dependencies
  */
 export async function verifyProjectDependencies() {
-  const dependencies = ["@astrojs/tailwind", "tailwind-merge", "class-variance-authority"];
+  const dependencies = [
+    '@astrojs/tailwind',
+    'tailwind-merge',
+    'class-variance-authority',
+  ]
   for (const dependency of dependencies) {
-    checkAndInstallPackage(dependency);
+    checkAndInstallPackage(dependency)
   }
 }
 
@@ -26,9 +35,13 @@ export async function verifyProjectDependencies() {
  */
 export async function checkAndInstallPackage(packageName) {
   if (!checkPackageExists(packageName)) {
-    console.log(chalk.yellow(`📦 ${packageName} is missing from your package.json. Installing...`));
-    execSync(`npm i ${packageName}`, { stdio: "inherit" });
-    console.log(success("✅ Installed dependency", packageName));
+    console.log(
+      chalk.yellow(
+        `📦 ${packageName} is missing from your package.json. Installing...`
+      )
+    )
+    execSync(`npm i ${packageName}`, { stdio: 'inherit' })
+    console.log(success('✅ Installed dependency', packageName))
   }
 }
 
@@ -37,34 +50,41 @@ export async function checkAndInstallPackage(packageName) {
  *
  * @param {object} answers
  */
-export async function verifyCompilerOptions(answers) {
-  const configFile = findConfigFile();
+export async function verifyCompilerOptions(answers, options) {
+  // TODO: options is not used. Consider -y
+  const configFile = findConfigFile()
 
   if (!configFile.compilerOptions) {
     await inquirer
       .prompt([
         {
-          type: "confirm",
-          name: "addCompilerOptions",
-          message: "Missing compilerOptions in your tsconfig.json. Should i add it for you?",
+          type: 'confirm',
+          name: 'addCompilerOptions',
+          message:
+            'Missing compilerOptions in your tsconfig.json. Should i add it for you?',
           default: true,
         },
       ])
       .then((_answers) => {
-        console.log(_answers.addCompilerOptions);
         if (_answers.addCompilerOptions) {
           const newConfig = {
             ...configFile,
             compilerOptions: {
-              baseUrl: ".",
+              baseUrl: '.',
               paths: {
-                [`${answers.aliasComponents}/*`]: [`${answers.outputComponents}/*`],
+                [`${answers.aliasComponents}/*`]: [
+                  `${answers.outputComponents}/*`,
+                ],
               },
             },
-          };
+          }
 
-          fs.writeFileSync(localPath("tsconfig.json"), JSON.stringify(newConfig, null, 2), "utf-8");
-          console.log(success("\n✅ Added compilerOptions to tsconfig.json"));
+          fs.writeFileSync(
+            localPath('tsconfig.json'),
+            JSON.stringify(newConfig, null, 2),
+            'utf-8'
+          )
+          console.log(success('\n✅ Added compilerOptions to tsconfig.json'))
         }
       })
       .catch((error) => {
@@ -73,7 +93,7 @@ export async function verifyCompilerOptions(answers) {
         } else {
           // Something else went wrong
         }
-      });
+      })
   }
 }
 
@@ -83,12 +103,12 @@ export async function verifyCompilerOptions(answers) {
  * @param {object} answers
  */
 export async function writeConfigFile(answers) {
-  const templatePath = path.join(__dirname, "templates", "ui-config.json");
-  const template = fs.readFileSync(templatePath, "utf-8");
-  const compiledTemplate = handlebars.compile(template);
-  const output = compiledTemplate(answers);
+  const templatePath = path.join(__dirname, 'templates', 'ui-config.json')
+  const template = fs.readFileSync(templatePath, 'utf-8')
+  const compiledTemplate = handlebars.compile(template)
+  const output = compiledTemplate(answers)
 
-  const filePath = localPath(`ui-config.json`);
-  fs.writeFileSync(filePath, output, "utf-8");
-  console.log(success("✅ Created ui-config.json"));
+  const filePath = localPath(`ui-config.json`)
+  fs.writeFileSync(filePath, output, 'utf-8')
+  console.log(success('✅ Created ui-config.json'))
 }
