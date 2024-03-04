e1a416c4842be2ad7de31ddb4c5d647db4de51e2
Dennis Söderström <dennis.soderstrom@gmail.com>
2024-03-05 00:07:45 +0100

    fix(docs): theming docs

--- apps/docs/tailwind.config.mjs
+++ apps/docs/tailwind.config.mjs
@@ -1,10 +1,28 @@
 import starlightPlugin from '@astrojs/starlight-tailwind'
+import colors from 'tailwindcss/colors'
 
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
-    extend: {},
+    fontFamily: {
+      sans: ['Graphik', 'sans-serif'],
+      serif: ['Merriweather', 'serif'],
+    },
+    extend: {
+      colors: {
+        primary: colors.indigo,
+        secondary: colors.yellow,
+        neutral: colors.gray,
+      },
+      spacing: {
+        '8xl': '96rem',
+        '9xl': '128rem',
+      },
+      borderRadius: {
+        '4xl': '2rem',
+      },
+    },
   },
   plugins: [starlightPlugin()],
 }
